b6b38345acfea27773ceac5aaff211f14b26d467
liyangyang <liyyco@126.com>
2017-12-15 18:15:51 +0800

    项目初始化

---
+++ src/main/java/com/lyyco/rays/RaysApplication.java
@@ -0,0 +1,13 @@
+package com.lyyco.rays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaysApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RaysApplication.class, args);
+	}
+}
