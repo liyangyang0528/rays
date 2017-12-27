822dd0b1131410c594630ea4a6241adbecf955eb
liyangyang <liyyco@126.com>
2017-12-27 18:54:57 +0800

    添加sparkConf

---
+++ src/main/java/com/lyyco/rays/service/spark/HelloSpark.java
@@ -0,0 +1,11 @@
+package com.lyyco.rays.service.spark;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+/**
+ * Created by lyy on 2017/12/27.
+ */
+public class HelloSpark {
+    org.apache.spark.SparkConf conf = new org.apache.spark.SparkConf().setAppName("lyy").setMaster(" http://192.168.137.1:4040");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+}
