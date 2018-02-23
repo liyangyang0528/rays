8cbb82dd5ca351d2f8adbdfd8f98bdf5c159b753
liyangyang <liyyco@126.com>
2018-02-23 16:57:26 +0800

    Stream test

--- src/main/java/com/lyyco/rays/service/lambda/StreamTest.java
+++ src/main/java/com/lyyco/rays/service/lambda/StreamTest.java
@@ -2,6 +2,8 @@ package com.lyyco.rays.service.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by lyy on 2018/2/23.
@@ -12,5 +14,26 @@ public class StreamTest {
         numbers.stream().filter(i -> i%2 == 0)
                 .distinct()
                 .forEach(System.out::println);
+
+        List<String> words = Arrays.asList("java8","Lambdas","In Action");
+        List<Integer> wordLength = words.stream().map(String::length).collect(Collectors.toList());
+        //flatMap
+        words.stream()
+                .map(word -> word.split(""))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
+        //实现对两个list的排列组合
+        List<Integer> num1 = Arrays.asList(1,2,3);
+        List<Integer> num2 = Arrays.asList(3,4);
+        List<int[]> pairs = num1.stream()
+                .flatMap(i -> num2.stream()
+                        //.filter(j -> (i+j)%3 == 0)
+                        .map(j -> new int[]{i,j})
+        ).collect(Collectors.toList());
+
+
+        Stream<String> stream = Stream.of("java8","Lambda");
+        stream.map(String::toUpperCase).forEach(System.out::println);
     }
 }
