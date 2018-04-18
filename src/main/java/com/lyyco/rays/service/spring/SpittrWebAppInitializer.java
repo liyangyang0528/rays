082f33039e869b29b90909d8ffcc0228c476cd96
liyangyang <liyyco@126.com>
2018-04-18 18:42:32 +0800

    spring DispatcherServlet's config

---
+++ src/main/java/com/lyyco/rays/service/spring/SpittrWebAppInitializer.java
@@ -0,0 +1,25 @@
+package com.lyyco.rays.service.spring;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * configuring dispatcherServlet
+ * Author liyangyang
+ * 2018/4/18
+ */
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{};
+    }
+    //specify configuration class
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+    //map dispatcherServlet to /
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
