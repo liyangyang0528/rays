b6ee21111617a5fece908792b2d6712f34a9eb81
liyangyang <liyyco@126.com>
2018-05-30 18:52:50 +0800

    自实现阻塞队列

---
+++ src/main/java/com/lyyco/rays/service/concurrent/BoundedBuffer.java
@@ -0,0 +1,25 @@
+package com.lyyco.rays.service.concurrent;
+
+/**
+ * Author liyangyang
+ * 2018/5/30
+ */
+public class BoundedBuffer<V> extends BaseBoundedBuffer<V> {
+
+    protected BoundedBuffer(int capacity) {
+        super(capacity);
+    }
+    public synchronized void put(V v) throws InterruptedException {
+        while (isFull())
+            wait();
+        doPut(v);
+        notifyAll();
+    }
+    public synchronized V take() throws InterruptedException {
+        while (isEmpty())
+            wait();
+        V v = doTake();
+        notifyAll();
+        return v;
+    }
+}
