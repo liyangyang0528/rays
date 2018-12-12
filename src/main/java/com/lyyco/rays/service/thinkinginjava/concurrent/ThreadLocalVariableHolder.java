5a84cd2b852b152393d2205b151f5aeb7d81fbf9
liyangyang <liyyco@126.com>
2018-12-12 18:54:16 +0800

    TIJ 21章 ThreadLocal

--- src/main/java/com/lyyco/rays/service/thinkinginjava/concurrent/ThreadLocalVariableHolder.java
+++ src/main/java/com/lyyco/rays/service/thinkinginjava/concurrent/ThreadLocalVariableHolder.java
@@ -1,30 +1,58 @@
 package com.lyyco.rays.service.thinkinginjava.concurrent;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 线程本地存储
  * Author liyangyang
  * 2018/12/12
  */
 public class ThreadLocalVariableHolder {
-    private static class Accessor implements Runnable{
+    private static class Accessor implements Runnable {
         private final int id;
-        public Accessor(int idn){
+
+        public Accessor(int idn) {
             id = idn;
         }
+
         @Override
         public void run() {
-            while (!Thread.currentThread().isInterrupted()){
+            while (!Thread.currentThread().isInterrupted()) {
                 ThreadLocalVariableHolder.increment();
                 System.out.println(this);
                 Thread.yield();
             }
         }
-        public String toString(){
+
+        public String toString() {
             return "#" + id + ":" + ThreadLocalVariableHolder.get();
         }
     }
-    private static ThreadLocal<Integer> value = new ThreadLocal<>();
+
+    private static ThreadLocal<Integer> value = new ThreadLocal<Integer>() {
+        private Random rand = new Random(47);
+
+        protected synchronized Integer initialValue() {
+            return rand.nextInt(1000);
+        }
+    };
+
     private static void increment() {
-        
+        value.set(value.get() + 1);
+    }
+    public static int get(){
+        return value.get();
+    }
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0;i<5;i++){
+            exec.execute(new Accessor(i));
+        }
+        TimeUnit.SECONDS.sleep(3);
+        exec.shutdown();
     }
 }
