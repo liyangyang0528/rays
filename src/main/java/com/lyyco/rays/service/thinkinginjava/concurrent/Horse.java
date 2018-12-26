7f27ccccaadff4eb7c4879b7ed579d0a6cce3f0d
liyangyang <liyyco@126.com>
2018-12-26 19:02:42 +0800

    使用CyclicBarrier

---
+++ src/main/java/com/lyyco/rays/service/thinkinginjava/concurrent/Horse.java
@@ -0,0 +1,53 @@
+package com.lyyco.rays.service.thinkinginjava.concurrent;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Author liyangyang
+ * 2018/12/26
+ */
+public class Horse implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private int strides = 0;
+    private static Random rand = new Random(47);
+    private static CyclicBarrier barrier;
+
+    public Horse(CyclicBarrier b) {
+        barrier = b;
+    }
+
+    public synchronized int getStrides() {
+        return strides;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    strides += rand.nextInt(3);
+                }
+                barrier.await();
+            }
+        } catch (InterruptedException e) {
+
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public String toString(){
+        return "Horse " + id + " ";
+    }
+    public String tracks(){
+        StringBuilder s = new StringBuilder();
+        for(int i = 0;i<getStrides();i++){
+            s.append("*");
+        }
+        s.append(id);
+        return s.toString();
+
+    }
+}
---
+++ src/main/java/com/lyyco/rays/service/thinkinginjava/concurrent/HorseRace.java
@@ -0,0 +1,8 @@
+package com.lyyco.rays.service.thinkinginjava.concurrent;
+
+/**
+ * Author liyangyang
+ * 2018/12/26
+ */
+public class HorseRace {
+}
